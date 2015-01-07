2bad9ed6bfb28a23c4a7dd99702bb09ad80cd6b6
everaldomatias <matiaseveraldo@gmail.com>
2015-01-07 13:04:30 -0200

    Adiciona codigo Google Analytics

--- functions.php
+++ functions.php
@@ -241,6 +241,21 @@ function thumbnail_bg ( $tamanho = 'thumbnail' ) {
     echo 'style="background: url('.$get_post_thumbnail[0].' )"';
 }
 
+/* Função para aplicar Google Analytics no Footer. */
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { ?>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-17302058-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
+<?php } 
 
 
-?>
